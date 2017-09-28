655603313c1d4881510a981042aecd02b784327a
Guest User <Guest@Epicodus-7G.local>
2017-09-28 09:51:37 -0700

    fix karma error

--- spec/time-spec.js
+++ spec/time-spec.js
@@ -1,6 +1,6 @@
 import {TimeCalc} from './../js/time.js'
 
-describe(TimeCalc, function(){
+describe('TimeCalc', function(){
   it('should test for conversion of date entered to Unix epoch',function(){
     let testAge = new TimeCalc(new Date("2000-09-08"))
     expect(testAge.findSeconds()).toEqual(968371200)
